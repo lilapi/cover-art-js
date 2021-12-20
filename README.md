d3fc4d7e1ad08179bcda26af624995a5513ac62b
Patrick Smith <pgwsmith@gmail.com>
2021-12-20 15:45:40 +1100

    Add usage to readme

--- README.md
+++ README.md
@@ -3,6 +3,7 @@
 Generate beautiful images on the fly.
 
 Works with:
+
 - Next.js
 - Remix
 - Express
@@ -12,3 +13,41 @@ Works with:
 ```bash
 npm add @littleeagle/node
 ```
+
+## Usage
+
+1. [Sign up](https://littleeagle.io/api/auth/signin) to create a free account on
+   [littleeagle.io](https://littleeagle.io/)
+2. Find your project ID and secret in the Little Eagle dashboard. Set these as
+   environment variables (suggested names: `LITTLE_EAGLE_PROJECT_ID` and
+   `LITTLE_EAGLE_PROJECT_SECRET`).
+   - Here’s how you
+     [add environment variables using Vercel](https://vercel.com/support/articles/how-to-add-vercel-environment-variables).
+3. Install the Little Eagle node package: `npm add @littleeagle/node`
+4. Call the `littleEagleImagesURL()` function to generate your images URL. An example is provided below. It’s
+   recommended you do this on the server as your secret shouldn’t be made
+   public.
+
+```ts
+const imageURL = littleEagleImagesURL({
+  id: process.env.LITTLE_EAGLE_PROJECT_ID,
+  secret: process.env.LITTLE_EAGLE_PROJECT_SECRET,
+}, {
+  template: "plain", // "plain" or "overlay" supported.
+  // Any of the below options can be dynamic.
+  // e.g. Load your text or image from your database or content management system!
+  backgroundColor: "#00b4ff",
+  imageURL:
+    "https://images.unsplash.com/photo-1606890658317-7d14490b76fd?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTJ8fHNuZWFrZXJ8ZW58MHx8MHx8&auto=format&fit=crop&w=600&q=60",
+  text: [
+    { text: "First", size: 17, color: "#aaa" },
+    { text: "Second", size: 23, color: "#bbb" },
+  ],
+});
+
+// Use `imageURL` e.g. as the value in a `og:image` meta tag.
+```
+
+## Coming soon
+
+An example repo in Next.js with Little Eagle integrated.
--- src/test.ts
+++ src/test.ts
@@ -7,24 +7,28 @@ test("littleEagleImagesURL()", () => {
     littleEagleImagesURL({ id: "abc", secret: "xyz" }, {
       template: "plain",
       backgroundColor: "#00b4ff",
+      imageURL:
+        "https://images.unsplash.com/photo-1606890658317-7d14490b76fd?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTJ8fHNuZWFrZXJ8ZW58MHx8MHx8&auto=format&fit=crop&w=600&q=60",
       text: [
         { text: "First", size: 17, color: "#aaa" },
         { text: "Second", size: 23, color: "#bbb" },
       ],
     }),
-    "https://cdn.littleeagle.io/1/p/abc/plain?t1=First&t1-size=17&t1-color=%23aaa&t2=Second&t2-size=23&t2-color=%23bbb&bg-color=%2300b4ff&s=c950e58a395f384b6d6309a2ad0566db564fa11169a10c544e37540175fc91ac",
+    "https://cdn.littleeagle.io/1/p/abc/plain?t1=First&t1-size=17&t1-color=%23aaa&t2=Second&t2-size=23&t2-color=%23bbb&bg-color=%2300b4ff&img=https%3A%2F%2Fimages.unsplash.com%2Fphoto-1606890658317-7d14490b76fd%3Fixlib%3Drb-1.2.1%26ixid%3DMnwxMjA3fDB8MHxzZWFyY2h8MTJ8fHNuZWFrZXJ8ZW58MHx8MHx8%26auto%3Dformat%26fit%3Dcrop%26w%3D600%26q%3D60&s=9928519b717414dce664a7a180150e9e0db659f3519a0dd0d008567b2f07eedb",
   );
 
   assert.is(
     littleEagleImagesURL({ id: "abc", secret: "xyz" }, {
       template: "overlay",
       backgroundColor: "#00b4ff",
+      imageURL:
+        "https://images.unsplash.com/photo-1606890658317-7d14490b76fd?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTJ8fHNuZWFrZXJ8ZW58MHx8MHx8&auto=format&fit=crop&w=600&q=60",
       text: [
         { text: "First", size: 17, color: "#aaa" },
         { text: "Second", size: 23, color: "#bbb" },
       ],
     }),
-    "https://cdn.littleeagle.io/1/p/abc/overlay?t1=First&t1-size=17&t1-color=%23aaa&t2=Second&t2-size=23&t2-color=%23bbb&bg-color=%2300b4ff&s=63cb87aff503b92c46a192cfb546634ac96eef71aee3424803eb69ac8b25bdf1",
+    "https://cdn.littleeagle.io/1/p/abc/overlay?t1=First&t1-size=17&t1-color=%23aaa&t2=Second&t2-size=23&t2-color=%23bbb&bg-color=%2300b4ff&img=https%3A%2F%2Fimages.unsplash.com%2Fphoto-1606890658317-7d14490b76fd%3Fixlib%3Drb-1.2.1%26ixid%3DMnwxMjA3fDB8MHxzZWFyY2h8MTJ8fHNuZWFrZXJ8ZW58MHx8MHx8%26auto%3Dformat%26fit%3Dcrop%26w%3D600%26q%3D60&s=79ece2529fecefcdc25e7eb65c540e2e4b650c3ed5362ff35c0f66b2ba935701",
   );
 });
 
